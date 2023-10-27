8e11cf52dbd1596304e491609f727396bb0e9229
Ferst1 <helenochka4@gmail.com>
2023-10-27 20:07:42 +0200

    updatad
    Styled

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,17 @@
+import propTypes from 'prop-types';
+import { ImageGalleryItems } from './ImageGalleryItem.styled';
+
+export const ImageGalleryItem = ({ largeImageLink, imageLink, onClick }) => {
+  return (
+    <ImageGalleryItems onClick={() => onClick(largeImageLink)}>
+      <img src={imageLink} alt="images" />
+    </ImageGalleryItems>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  largeImageLink: propTypes.string.isRequired,
+  imageLink: propTypes.string.isRequired,
+  imageLink: propTypes.string.isRequired,
+  onClick: propTypes.func.isRequired,
+};
